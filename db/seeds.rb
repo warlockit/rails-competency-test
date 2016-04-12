c4b4462c3d8e755c859c5449542fa2996a7beb4e
warlockit <warlockit@yandex.ru>
2016-04-12 15:10:33 +0900

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+if User.find_by_email("warlockit@yandex.ru").nil?
+  user = User.create!({:email => "warlockit@yandex.ru", :password => "testpass", :password_confirmation => "testpass"})
+  user.roles = :admin
+  user.save
+end
+
+unless Category.all.any?
+  Category.create!({:title => "First category"})
+  Category.create!({:title => "Second category"})
+  Category.create!({:title => "Third category"})
+end
