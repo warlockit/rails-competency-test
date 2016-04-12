f190e6f68eb979a48dabdf3bc061cd1731875e41
warlockit <warlockit@yandex.ru>
2016-04-12 15:10:18 +0900

    Set default role for new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,17 @@
 class User < ActiveRecord::Base
+  petergate(roles: [:user, :editor, :admin])
+
+  after_initialize :set_default_role, :if => :new_record?
+ 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :articles
+
+  def set_default_role
+    self.roles ||= :user
+  end
+
 end
